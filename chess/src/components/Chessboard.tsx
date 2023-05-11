19823ba06e52ac3a3707e9f2ec0c81dc45f2bb58
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-11 15:48:41 +0530

    done

--- chess/src/components/Chessboard.tsx
+++ chess/src/components/Chessboard.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './ChessBoard.css';
+const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const horizontalaxis = ["a","b","c","d","e","f","g","h"];
 export default function Chessboard() {
-    return <div>hello niggas</div>
+    return <div id="chessboard">hhhhh</div>;
+    
 }
