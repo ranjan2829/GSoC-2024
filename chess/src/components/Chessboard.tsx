79a6f1a7897794d251a4250dd0f14dfa65b15f8b
ranjan2829 <ranjan.shitole3129@gmail.com>
2023-05-11 15:58:45 +0530

    done

--- chess/src/components/Chessboard.tsx
+++ chess/src/components/Chessboard.tsx
@@ -3,6 +3,5 @@ import './ChessBoard.css';
 const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
 const horizontalaxis = ["a","b","c","d","e","f","g","h"];
 export default function Chessboard() {
-    return <div id="chessboard">hhhhh</div>;
-    
+    return <div id="chessboard"></div>;
 }
